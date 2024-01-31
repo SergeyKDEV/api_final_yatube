411c9e4bf8c7e4851acbb1311c78597b50ae4cac
Sergey Kulbida <kulbida.sergey@outlook.com>
2024-02-01 06:51:00 +0700

    added perform_create method, permissions, pagination for PostViewSet

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,4 +1,10 @@
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+
+from posts.models import Post
+
+from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer
 
 
 class PostViewSet(ModelViewSet):
@@ -14,7 +20,14 @@ class PostViewSet(ModelViewSet):
     - DELETE.
     """
 
-    queryset = None
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+    pagination_class = (LimitOffsetPagination,)
+
+    def perform_create(self, serializer):
+        """Создает пост с автором из запроса."""
+        serializer.save(author=self.request.user)
 
 
 class GroupViewSet(ReadOnlyModelViewSet):
