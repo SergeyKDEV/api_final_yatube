05f764757d3c56d9a5ddc652b5c20b118e71c50a
Sergey Kulbida <kulbida.sergey@outlook.com>
2024-01-31 16:25:12 +0700

    added urls and created views classes

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,17 @@
-# TODO:  Напишите свой вариант
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+
+
+class PostViewSet(ModelViewSet):
+    queryset = None
+
+
+class GroupViewSet(ReadOnlyModelViewSet):
+    queryset = None
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = None
+
+
+class FollowViewSet(ModelViewSet):
+    queryset = None
